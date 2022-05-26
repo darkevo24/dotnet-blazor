1339417fb958bf9856d3fa6606fdbab85ba7c156
darkevo2424 <timotius.albert@luxpmsoft.com>
2022-05-26 15:39:39 +0700

    done

---
+++ Data/ApplicantListModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Recruitment.Data
+{
+    public class ApplicantListModel
+    {
+        public int No { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Gender { get; set; }
+        public string MaritasStatus { get; set; }
+        public string Religion { get; set; }
+        public DateTime Date { get; set; }
+        public int ID { get; set; }
+
+
+    }
+}
---
+++ Data/VacancyListModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Recruitment.Data
+{
+    public class VacancyListModel
+    {
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public int NoEmpl { get; set; }
+        public int ID {get; set;}
+        public string Status { get; set; }  
+
+
+    }
+}
